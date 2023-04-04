b2a5a8f8a3575ce67c8cbaf68f2abb91d4219af3
Zach Schwark <zachs1708@gmail.com>
2023-04-04 23:18:43 +0200

    login comments

--- lib/screens/authentication/login_manager.dart
+++ lib/screens/authentication/login_manager.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-
 import 'package:crypto/crypto.dart';
 import 'package:sps_app/http_handler.dart';
 
+// class to manage the user account's details and processes, and access to the app.
 class LoginManager {
   LoginManager();
 
@@ -25,12 +25,14 @@ class LoginManager {
     return _hashPassword().toString();
   }
 
+  // to control access into the app by validating credentials with backend
   static Future<bool> validateLogin() async {
     bool status =
         await HTTPManager.postLoginCredentials(getUsername(), getPassword());
     return status;
   }
 
+  //hash password for secure data transfer over the internet.
   static Digest _hashPassword() {
     var bytes = utf8.encode(password);
     return sha256.convert(bytes);
