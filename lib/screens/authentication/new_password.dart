b6ec2a452456a28cc1e367b0333a3ca66fc9d53a
Lisa Godwin <lisa.godwin2002@gmail.com>
2023-04-06 13:39:17 +0200

    Add OTP UI

---
+++ lib/screens/authentication/new_password.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewPasswordPage extends StatefulWidget {
+  const NewPasswordPage({super.key});
+
+  @override
+  State<NewPasswordPage> createState() => _NewPasswordPageState();
+}
+
+class _NewPasswordPageState extends State<NewPasswordPage>{
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+
+    );
+  }
+}
