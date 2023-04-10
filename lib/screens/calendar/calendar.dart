8517c1a974157aa6c99918c26af169ad1846bb99
nihal-ranchod <nihal.ranchod@gmail.com>
2023-04-10 17:02:21 +0200

    Calendar UI Change
    Add: change Calendar UI so that it can accommodate functionality for backend and display corresponding dots

--- lib/screens/calendar/calendar.dart
+++ lib/screens/calendar/calendar.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:scrollable_clean_calendar/controllers/clean_calendar_controller.dart';
-import 'package:scrollable_clean_calendar/scrollable_clean_calendar.dart';
-import 'package:scrollable_clean_calendar/utils/enums.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({Key? key}) : super(key: key);
@@ -10,43 +8,109 @@ class CalendarPage extends StatefulWidget {
   State<CalendarPage> createState() => _CalendarPageState();
 }
 
+/// The app which hosts the home page which contains the calendar on it.
+class CalendarApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Calendar Demo', home: CalendarPage());
+  }
+}
+
 //initialises the calendar page and is used in the nav bar as one of the pages
 class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Calendar', home: ScrollableCalendar());
+    return Scaffold(
+        body: SfCalendar(
+      view: CalendarView.month,
+      monthViewSettings: const MonthViewSettings(
+        navigationDirection: MonthNavigationDirection.vertical,
+        monthCellStyle: MonthCellStyle(),
+        showTrailingAndLeadingDates: false,
+        appointmentDisplayMode: MonthAppointmentDisplayMode.indicator,
+        showAgenda: true,
+        agendaStyle:
+            const AgendaStyle(appointmentTextStyle: TextStyle(fontSize: 15)),
+      ),
+      dataSource: MeetingDataSource(_getDataSource()),
+      // by default the month appointment display mode set as Indicator, we can
+      // change the display mode as appointment using the appointment display
+      // mode property
+    ));
+  }
+
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+    final DateTime startTime = DateTime(today.year, today.month, today.day, 9);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+    meetings.add(
+        Meeting('Hello', startTime, endTime, Color.fromARGB(255, 12, 227, 80)));
+    meetings.add(Meeting(
+        'Rotation', startTime, endTime, Color.fromARGB(255, 134, 15, 78)));
+    return meetings;
   }
 }
 
-class ScrollableCalendar extends StatelessWidget {
-  final calendarController = CleanCalendarController(
-    minDate: DateTime.now().subtract(Duration(days: 365)),
-    maxDate: DateTime.now().add(const Duration(days: 365)),
-    onRangeSelected: (firstDate, secondDate) {},
-    onDayTapped: (date) {},
-    onPreviousMinDateTapped: (date) {},
-    onAfterMaxDateTapped: (date) {},
-    weekdayStart: DateTime.monday,
-    // initialDateSelected: DateTime(2022, 2, 3),
-    // endDateSelected: DateTime(2022, 2, 3),
-  );
+// An object to set the appointment collection data source to calendar, which
+/// used to map the custom appointment data to the calendar appointment, and
+/// allows to add, remove or reset the appointment collection.
+class MeetingDataSource extends CalendarDataSource {
+  /// Creates a meeting data source, which used to set the appointment
+  /// collection to the calendar
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFFFFFFF),
-      body: ScrollableCleanCalendar(
-        calendarController: calendarController,
-        layout: Layout.DEFAULT,
-        calendarCrossAxisSpacing: 0,
-        spaceBetweenMonthAndCalendar: 20,
-        monthTextStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        monthTextAlign: TextAlign.left,
-        daySelectedBackgroundColor: Color(0xFF917248),
-        dayBackgroundColor: Color(0xFFFFFFFF),
-        dayRadius: 30,
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
-      ),
-    );
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
   }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return _getMeetingData(index).background;
+  }
+
+  Meeting _getMeetingData(int index) {
+    final dynamic meeting = appointments![index];
+    late final Meeting meetingData;
+    if (meeting is Meeting) {
+      meetingData = meeting;
+    }
+
+    return meetingData;
+  }
+}
+
+class Meeting {
+  /// Creates a meeting class with required details.
+  Meeting(
+    this.eventName,
+    this.from,
+    this.to,
+    this.background,
+  );
+
+  /// Event name which is equivalent to subject property of [Appointment].
+  String eventName;
+
+  /// From which is equivalent to start time property of [Appointment].
+  DateTime from;
+
+  /// To which is equivalent to end time property of [Appointment].
+  DateTime to;
+
+  /// Background which is equivalent to color property of [Appointment].
+  Color background;
 }
