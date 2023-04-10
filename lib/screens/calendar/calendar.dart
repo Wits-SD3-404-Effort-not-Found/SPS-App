576f1085f221f0d79ecc74e6c2493ada3d291229
Ashlea <smithashlea@gmail.com>
2023-04-10 14:48:11 +0200

    Calendar UI
    Add: scrollable Calendar UI with all months

--- lib/screens/calendar/calendar.dart
+++ lib/screens/calendar/calendar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_clean_calendar/controllers/clean_calendar_controller.dart';
+import 'package:scrollable_clean_calendar/scrollable_clean_calendar.dart';
+import 'package:scrollable_clean_calendar/utils/enums.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({Key? key}) : super(key: key);
@@ -11,12 +14,31 @@ class CalendarPage extends StatefulWidget {
 class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-          padding: EdgeInsets.only(bottom: 25),
-          child: Align(
-              alignment: Alignment.bottomCenter, child: Text('Calendar Page'))),
+    return MaterialApp(title: 'Calendar', home: ScrollableCalendar());
+  }
+}
+
+class ScrollableCalendar extends StatelessWidget {
+  final calendarController = CleanCalendarController(
+    minDate: DateTime.now(),
+    maxDate: DateTime.now().add(const Duration(days: 365)),
+    onRangeSelected: (firstDate, secondDate) {},
+    onDayTapped: (date) {},
+    onPreviousMinDateTapped: (date) {},
+    onAfterMaxDateTapped: (date) {},
+    weekdayStart: DateTime.monday,
+    // initialDateSelected: DateTime(2022, 2, 3),
+    // endDateSelected: DateTime(2022, 2, 3),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ScrollableCleanCalendar(
+        calendarController: calendarController,
+        layout: Layout.BEAUTY,
+        calendarCrossAxisSpacing: 0,
+      ),
     );
   }
 }
