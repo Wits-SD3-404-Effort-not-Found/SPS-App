efc87510633049f089f26faee950662e991d7a7f
Ashlea <smithashlea@gmail.com>
2023-04-19 14:25:33 +0200

    Calendar manager page
    Add: getting account id from the login manager

--- lib/screens/calendar/calendar_manager.dart
+++ lib/screens/calendar/calendar_manager.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:sps_app/screens/authentication/login_manager.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 final String serverAddress = '164.92.183.156';
@@ -8,15 +9,15 @@ final String serverPort = '80';
 
 class Events {
   Events(
-      {this.event_id,
-      this.account_id,
+      {required this.event_id,
+      required this.account_id,
       this.start_date,
       this.end_date,
       this.name,
       this.description});
 
-  int? event_id;
-  int? account_id; //need to get account id from the login
+  int event_id;
+  int account_id; //need to get account id from the login
   DateTime? start_date;
   DateTime? end_date;
   String? name;
@@ -35,7 +36,8 @@ Future<List<Events>> getEventsData() async {
   for (var data in jsonData) {
     Events eventData = Events(
         event_id: data['event_id'],
-        account_id: data['account_id'], //will need to change
+        account_id:
+            LoginManager.getAccountID(), // check this actually gets the ID
         start_date: _convertDateFromString(data['start_date']),
         end_date: _convertDateFromString(data['end_date']),
         name: data['name'],
