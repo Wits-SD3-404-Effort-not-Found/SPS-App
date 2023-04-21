2f2ece817a8b5be4896ef10b04535b3caf8b5c14
Lisa Godwin <lisa.godwin2002@gmail.com>
2023-04-21 14:12:37 +0200

    add new notes.dart

--- lib/screens/notes/notes.dart
+++ lib/screens/notes/notes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sps_app/screens/notes/protocols.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({Key? key}) : super(key: key);
@@ -10,12 +11,80 @@ class NotesPage extends StatefulWidget {
 class _NotesPageState extends State<NotesPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-          padding: EdgeInsets.only(bottom: 25),
-          child: Align(
-              alignment: Alignment.bottomCenter, child: Text('Notes Page'))),
-    );
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(children: <Widget>[
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+              child: Text(
+                'Notes',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(children: [
+              ConstrainedBox(
+                  constraints: BoxConstraints.tight(const Size(250, 50)),
+                  child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ProtocolsPage()),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                          side: const BorderSide(color: Color(0xff043673))),
+                      child: Row(children: const [
+                        Text(
+                          'Protocols',
+                          style: TextStyle(color: Colors.black, fontSize: 24),
+                        ),
+                        Icon(
+                          Icons.arrow_right,
+                          size: 45,
+                          color: Colors.black,
+                        ),
+                      ]))),
+              ConstrainedBox(
+                  constraints: BoxConstraints.tight(const Size(250, 50)),
+                  child: OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                          side: const BorderSide(color: Color(0xff043673))),
+                      child: Row(children: const [
+                        Text(
+                          'Notes',
+                          style: TextStyle(color: Colors.black, fontSize: 24),
+                        ),
+                        Icon(
+                          Icons.arrow_right,
+                          size: 45,
+                          color: Colors.black,
+                        ),
+                      ]))),
+              ConstrainedBox(
+                  constraints: BoxConstraints.tight(const Size(250, 50)),
+                  child: OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                          side: const BorderSide(color: Color(0xff043673))),
+                      child: Row(children: const [
+                        Text(
+                          'Logbook',
+                          style: TextStyle(color: Colors.black, fontSize: 24),
+                        ),
+                        Icon(
+                          Icons.arrow_right,
+                          size: 45,
+                          color: Colors.black,
+                        ),
+                      ]))),
+            ]),
+          )
+        ]));
   }
 }
