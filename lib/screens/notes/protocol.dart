4afdb15fbb73cdafb65d6b0e032c400fd9c25dfa
Lisa Godwin <lisa.godwin2002@gmail.com>
2023-04-22 14:55:52 +0200

    add protocol functionality

--- lib/screens/notes/protocol.dart
+++ lib/screens/notes/protocol.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sps_app/http_handler.dart';
 import '../../widgets/primitive/list_item.dart';
 
 class ProtocolScreen extends StatefulWidget {
@@ -9,46 +10,82 @@ class ProtocolScreen extends StatefulWidget {
 }
 
 class ProtocolScreenState extends State<ProtocolScreen> {
-  final List<ListItem> items = [
-    ProtocolItem("Example Protocol", "Run burn wound under cold water. keep under water until its cooled down"),
-    ProtocolItem("Gun", "call the MF-ing Police"),
-  ];
+  var items =[];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 55,
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 8),
-            decoration: const BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(color: Color(0xff917248), width: 2)
-                )
-            ),
-            child: const Text(
-              "Protocols", style: TextStyle(fontSize: 30),
-            ),
-          ),
-          Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.only(top:10),
-                // Let the ListView know how many items it needs to build.
-                itemCount: items.length,
-                // Provide a builder function. This is where the magic happens.
-                // Convert each item into a widget based on the type of item it is.
-                itemBuilder: (BuildContext context, int index) {
-                  final item = items[index];
-                  return ListTile(
-                    horizontalTitleGap: 0,
-                    title: item.buildItem(context),
-                  );
-                },
-              ),
-          )
-        ],
-      )
+    return FutureBuilder(
+      future: HTTPManager.getProtocols(),
+      builder: (BuildContext ctx, AsyncSnapshot snapshot){
+        if (snapshot.data == null){
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            )
+          );
+        } else{
+          items.clear();
+          for(var protocol in snapshot.data){
+            if(protocol["protocolTitle"] != null && protocol["protocolContent"] != null){
+              items.add(ProtocolItem(protocol["protocolTitle"], protocol["protocolContent"]));
+            }
+          }
+          return Scaffold(
+              body: Column(
+                children: <Widget>[
+                  Container(
+                      height: 50,
+                      width: double.infinity,
+                      alignment: Alignment.bottomLeft,
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(color: Color(0xff917248), width: 2))),
+                      child: Row(children: [
+                        Align(
+                            alignment: Alignment.center,
+                            child: Padding(
+                                padding:
+                                const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                                child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Icon(
+                                      Icons.arrow_back_ios_rounded,
+                                      size: 30,
+                                      color: Colors.black,
+                                    )))),
+                        Container(
+                            height: 50,
+                            width: 300,
+                            alignment: Alignment.centerLeft,
+                            child: const Text(
+                              "Protocols",
+                              style: TextStyle(fontSize: 30),
+                              textAlign: TextAlign.left,
+                            ))
+                      ])),
+                  Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.only(top:10),
+                      // Let the ListView know how many items it needs to build.
+                      itemCount: items.length,
+                      // Provide a builder function. This is where the magic happens.
+                      // Convert each item into a widget based on the type of item it is.
+                      itemBuilder: (BuildContext context, int index) {
+                        final item = items[index];
+                        return ListTile(
+                          horizontalTitleGap: 0,
+                          title: item.buildItem(context),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              )
+          );
+        }
+      }
     );
+
   }
 }
--- lib/screens/notes/single_protocol.dart
+++ lib/screens/notes/single_protocol.dart
@@ -11,18 +11,37 @@ class SingleProtocolPage extends StatelessWidget {
         body: Column(
             children: <Widget>[
               Container(
-                height: 55,
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 8),
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(color: Color(0xff917248), width: 2)
-                  )
-                ),
-                child: Text(
-                    protocolHeading, style: const TextStyle(fontSize: 30),
-                ),
-              ),
+                  height: 50,
+                  width: double.infinity,
+                  alignment: Alignment.bottomLeft,
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(color: Color(0xff917248), width: 2))),
+                  child: Row(children: [
+                    Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                            padding:
+                            const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Icon(
+                                  Icons.arrow_back_ios_rounded,
+                                  size: 30,
+                                  color: Colors.black,
+                                )))),
+                    Container(
+                        height: 50,
+                        width: 300,
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          protocolHeading,
+                          style:  const TextStyle(fontSize: 30),
+                          textAlign: TextAlign.left,
+                        ))
+                  ])),
              SizedBox(
                 width: double.infinity,
                 child: Padding(
