115b9491b85d2aa113852e1a354a3e3299323e8e
nihal-ranchod <nihal.ranchod@gmail.com>
2023-04-22 17:52:06 +0200

    Modal manager
    Add: modal manager to get events

---
+++ lib/screens/calendar/manager.dart
@@ -0,0 +1,49 @@
+import 'dart:collection';
+
+import 'package:sps_app/screens/calendar/calendar_manager.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+List<Events> getEventsModalData() {
+  List<Events> eventsList = <Events>[];
+
+  Events event1 = Events(
+      event_id: 1,
+      startDate: DateTime(2023, 4, 22, 8),
+      endDate: DateTime(2023, 4, 22, 10),
+      eventName: 'Tutorial',
+      description: 'Anatomy');
+  Events event2 = Events(
+      event_id: 2,
+      startDate: DateTime(2023, 4, 24),
+      endDate: DateTime(2023, 4, 26),
+      eventName: 'Assignment',
+      description: 'physiology');
+  eventsList.add(event1);
+  eventsList.add(event2);
+  return eventsList;
+}
+
+late LinkedHashMap<DateTime, List<Events>> allEvents =
+    LinkedHashMap(equals: isSameDay, hashCode: getHashCode);
+
+int getHashCode(DateTime key) {
+  return key.day * 1000000 + key.month * 10000 + key.year;
+}
+
+allTheEvents(List<Events> events) {
+  events.forEach((event) {
+    DateTime date = DateTime.utc(event.startDate!.year, event.startDate!.month,
+        event.startDate!.day, 12);
+    if (allEvents[date] == null) {
+      allEvents[date] = [];
+    }
+    allEvents[date]?.add(event);
+  });
+}
+
+List<DateTime> daysInRange(DateTime start, DateTime last) {
+  final dayCount = last.difference(start).inDays + 1;
+
+  return List.generate(dayCount,
+      (index) => DateTime.utc(start.year, start.month, start.day + index));
+}
--- lib/screens/calendar/modal.dart
+++ lib/screens/calendar/modal.dart
@@ -5,6 +5,8 @@ import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:sps_app/screens/calendar/calendar_manager.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
+import 'manager.dart';
+
 class ModalScreen extends StatefulWidget {
   const ModalScreen({Key? key}) : super(key: key);
 
@@ -13,8 +15,46 @@ class ModalScreen extends StatefulWidget {
 }
 
 class ModalScreenState extends State<ModalScreen> {
-  DateTime _selectedCalendarDate = DateTime.now();
-  DateTime _focusedCalendarDate = DateTime.now();
+  late final ValueNotifier<List<Events>> _selectedEvents;
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay = DateTime.now();
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
+
+  List<Events> getEventsForDay(DateTime date) {
+    return allEvents[date] ?? [];
+  }
+
+  List<Events> getEventsForRange(DateTime start, DateTime end) {
+    final days = daysInRange(start, end);
+
+    return [
+      for (final d in days) ...getEventsForDay(d),
+    ];
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, _focusedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _rangeStart = null;
+        _rangeEnd = null;
+      });
+      _selectedEvents.value = getEventsForDay(selectedDay); //not populating
+    }
+  }
+
+  final eventsList = getEventsModalData();
+
+  @override
+  void initState() {
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier(getEventsForDay(_selectedDay));
+    allTheEvents(eventsList);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,17 +110,6 @@ class ModalScreenState extends State<ModalScreen> {
                       color: Color(0xFF043673), shape: BoxShape.circle),
                   selectedDecoration: BoxDecoration(
                       color: Color(0xFF917248), shape: BoxShape.circle)),
-              selectedDayPredicate: (currentSelectedDate) {
-                return (isSameDay(_selectedCalendarDate!, currentSelectedDate));
-              },
-              onDaySelected: (selectedDay, focusedDay) {
-                if (!isSameDay(_selectedCalendarDate, selectedDay)) {
-                  setState(() {
-                    _selectedCalendarDate = selectedDay;
-                    _focusedCalendarDate = focusedDay;
-                  });
-                }
-              },
             ),
           ),
         ],
