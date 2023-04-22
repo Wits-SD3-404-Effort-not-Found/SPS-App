3a330e3c7fd6f233af3f0c8c1e69174b4dfc2903
Zach Schwark <120644105+Zach-schwark@users.noreply.github.com>
2023-04-22 03:04:20 +0200

    add auto save with my button

--- lib/screens/notes/single_note.dart
+++ lib/screens/notes/single_note.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sps_app/screens/notes/note_content.dart';
-import 'package:sps_app/screens/notes/personal_notes.dart';
 
 class SingleNotePage extends StatefulWidget {
   final NoteContent noteContent;
@@ -27,80 +26,81 @@ class _SingleNotePageState extends State<SingleNotePage> {
         text: widget.noteContent.getTitle(),
       ),
     );
+    widget.noteContent.setBody(_bodyController.text);
+    widget.noteContent.setTitle(_titleController.text);
   }
+
+  /*Future<bool> _autoSave() async {
+    debugPrint("in auto save");
+    widget.noteContent.setBody(_bodyController.text);
+    widget.noteContent.setTitle(_titleController.text);
+    Navigator.pop(context);
+    return true;
+  }*/
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(children: <Widget>[
-          Container(
-              height: 100,
-              width: double.infinity,
-              padding: const EdgeInsets.only(
-                  top: 40), // just for now for testing, bc top bar not here
-              alignment: Alignment.centerLeft,
-              decoration: const BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(color: Color(0xff917248), width: 2))),
-              child: Row(children: [
-                Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    child: GestureDetector(
-                        onTap: () {
-                          widget.noteContent.setBody(_bodyController.text);
-                          widget.noteContent.setTitle(_titleController.text);
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => PersonalNotesPage()));
-                        },
-                        child: const Icon(
-                          Icons.arrow_back_ios_rounded,
-                          size: 25,
-                          color: Colors.black,
-                        ))),
-                ConstrainedBox(
-                    constraints: BoxConstraints.tight(const Size(300, 50)),
-                    child: TextField(
-                      decoration:
-                          const InputDecoration(border: InputBorder.none),
-                      controller: _titleController,
-                      style: const TextStyle(fontSize: 30),
-                      textAlign: TextAlign.left,
-                    ))
-              ])),
-          Center(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: TextField(
-                decoration: const InputDecoration(border: InputBorder.none),
-                autofocus: true,
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                controller: _bodyController),
-          )),
-        ]));
+    return // WillPopScope(
+        //onWillPop: _autoSave,
+        //child:
+        Scaffold(
+            backgroundColor: Colors.white,
+            body: Column(children: <Widget>[
+              Container(
+                  height: 60,
+                  width: double.infinity,
+                  alignment: Alignment.bottomLeft,
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom:
+                              BorderSide(color: Color(0xff917248), width: 2))),
+                  child: Row(children: [
+                    Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            child: GestureDetector(
+                                onTap: () {
+                                  widget.noteContent
+                                      .setBody(_bodyController.text);
+                                  widget.noteContent
+                                      .setTitle(_titleController.text);
+                                  Navigator.pop(
+                                    context,
+                                  );
+                                },
+                                child: const Icon(
+                                  Icons.arrow_back_ios_rounded,
+                                  size: 30,
+                                  color: Colors.black,
+                                )))),
+                    Container(
+                        height: 60,
+                        width: 300,
+                        alignment: Alignment.bottomLeft,
+                        child: TextField(
+                          decoration:
+                              const InputDecoration(border: InputBorder.none),
+                          controller: _titleController,
+                          style: const TextStyle(fontSize: 30),
+                          textAlign: TextAlign.left,
+                        ))
+                  ])),
+              Center(
+                  child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: TextField(
+                    decoration: const InputDecoration(border: InputBorder.none),
+                    autofocus: true,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    controller: _bodyController),
+              )),
+            ]));
   }
 }
-
-/* Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 20),*/
-
-/*                  GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => PersonalNotesPage()));
-                      },
-                      child: const Icon(
-                        Icons.arrow_forward_ios_rounded,
-                        size: 20,
-                        color: Colors.black,
-                      )),*/
 
 /* Padding(
                   padding:
