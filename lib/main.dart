9124db3a880d343b7cb0ba9a15b2e067e5585bec
Orwellian <orwellian225@gmail.com>
2023-05-10 15:27:21 +0200

    add: Theme Objects

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:sps_app/screens/authentication/login.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:sps_app/account_manager.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -14,8 +15,82 @@ void main() async {
   AccountManager.setID(accountBox.get('account_id', defaultValue: 0));
 
   runApp(Phoenix(
-      child: const MaterialApp(
+      child: MaterialApp(
     title: 'SPS-App',
-    home: LoginPage(),
+    home: const LoginPage(),
+    theme: lightTheme(),
+    darkTheme: darkTheme(),
   )));
+}
+
+ThemeData lightTheme() {
+  return ThemeData(
+      colorScheme: lightColours(), textTheme: GoogleFonts.poppinsTextTheme());
+}
+
+ThemeData darkTheme() {
+  return ThemeData(
+      colorScheme: darkColours(), textTheme: GoogleFonts.poppinsTextTheme());
+}
+
+ColorScheme lightColours() {
+  const primaryColor = Color(0xff043673);
+  const onPrimaryColor = Color(0xffffffff);
+  const secondaryColor = Color(0xff917248);
+  const onSecondaryColor = Color(0xffffffff);
+  const tertiaryColor = Color(0xff2fac66);
+  const onTertiaryColor = Color(0xffffffff);
+  const errorColor = Color(0xffff0436);
+  const onErrorColor = Color(0xffffffff);
+  const backgroundColor = Color(0xffe8e8e8);
+  const onBackgroundColor = Color(0xff000000);
+  const surfaceColor = Color(0xffffffff);
+  const onSurfaceColor = Color(0xff000000);
+
+  return const ColorScheme(
+    brightness: Brightness.dark,
+    primary: primaryColor,
+    onPrimary: onPrimaryColor,
+    secondary: secondaryColor,
+    onSecondary: onSecondaryColor,
+    tertiary: tertiaryColor,
+    onTertiary: onTertiaryColor,
+    error: errorColor,
+    onError: onErrorColor,
+    background: backgroundColor,
+    onBackground: onBackgroundColor,
+    surface: surfaceColor,
+    onSurface: onSurfaceColor,
+  );
+}
+
+ColorScheme darkColours() {
+  const primaryColor = Color(0xff043673);
+  const onPrimaryColor = Color(0xffffffff);
+  const secondaryColor = Color(0xff917248);
+  const onSecondaryColor = Color(0xffffffff);
+  const tertiaryColor = Color(0xff2fac66);
+  const onTertiaryColor = Color(0xffffffff);
+  const errorColor = Color(0xffff0436);
+  const onErrorColor = Color(0xffffffff);
+  const backgroundColor = Color(0xffe8e8e8);
+  const onBackgroundColor = Color(0xff000000);
+  const surfaceColor = Color(0xffffffff);
+  const onSurfaceColor = Color(0xff000000);
+
+  return const ColorScheme(
+    brightness: Brightness.dark,
+    primary: primaryColor,
+    onPrimary: onPrimaryColor,
+    secondary: secondaryColor,
+    onSecondary: onSecondaryColor,
+    tertiary: tertiaryColor,
+    onTertiary: onTertiaryColor,
+    error: errorColor,
+    onError: onErrorColor,
+    background: backgroundColor,
+    onBackground: onBackgroundColor,
+    surface: surfaceColor,
+    onSurface: onSurfaceColor,
+  );
 }
