a266b764c41cb86eb2b98b99290a178a5ad26b3c
Lisa Godwin <lisa.godwin2002@gmail.com>
2023-05-12 13:09:22 +0200

    add account settings UI

--- lib/screens/settings/account_settings.dart
+++ lib/screens/settings/account_settings.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatefulWidget {
@@ -8,6 +10,12 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends State<AccountPage> {
+  var username = TextEditingController();
+  var cellNumber = TextEditingController();
+  var email = ''; //come back
+  final Uint8List photo = Uint8List(0);
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,6 +59,80 @@ class _AccountPageState extends State<AccountPage> {
                   )
               )
             ],
+            ),
+          ),
+          const Padding(
+            padding:
+            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          ),
+          Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              color: const Color(0xFF043673),
+              border: Border.all(
+                color: const Color(0xff917248),
+                width: 3.0,
+              ),
+              borderRadius: BorderRadius.circular(75),
+            ),
+            //child: CircleAvatar(
+              //radius: 25,
+              //backgroundImage: MemoryImage(bytes, {scale: 1}),
+            //),
+          ),
+          const Padding(
+            padding:
+            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          ),
+          Container(
+            height: 40,
+            width: 300,
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Color(0xff917248), width:2)
+              )
+            ),
+            child: const Text(
+                "username",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const Padding(
+            padding:
+            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          ),
+          Container(
+            height: 40,
+            width: 300,
+            decoration: const BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(color: Color(0xff917248), width:2)
+                )
+            ),
+            child: const Text(
+              "phone number",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const Padding(
+            padding:
+            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          ),
+          Container(
+            height: 40,
+            width: 300,
+            decoration: const BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(color: Color(0xff917248), width:2)
+                )
+            ),
+            child: const Text(
+              "email",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.left,
             ),
           )
         ],
