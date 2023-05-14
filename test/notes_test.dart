f1572a672eda5776c4b3ce17b9fa30c0b48f36da
Orwellian <orwellian225@gmail.com>
2023-05-14 21:16:57 +0200

    remove: Remove widget being tested, so remove test

--- test/notes_test.dart
+++ test/notes_test.dart
@@ -6,8 +6,7 @@ void main() {
   //testing that the text 'Notes' appears
   testWidgets('Testing Notes has text', (tester) async {
     Widget testWidget = const MediaQuery(
-        data: MediaQueryData(),
-        child: MaterialApp(home: NotesPage()));
+        data: MediaQueryData(), child: MaterialApp(home: NotesPage()));
 
     await tester.pumpWidget(testWidget);
 
@@ -19,8 +18,7 @@ void main() {
   //testing that the 'Protocols' button appears
   testWidgets('Testing Protocols button appears', (tester) async {
     Widget testWidget = const MediaQuery(
-        data: MediaQueryData(),
-        child: MaterialApp(home: NotesPage()));
+        data: MediaQueryData(), child: MaterialApp(home: NotesPage()));
 
     await tester.pumpWidget(testWidget);
 
@@ -32,27 +30,13 @@ void main() {
   //testing that the 'Personal Notes' button appears
   testWidgets('Testing Personal Notes button appears', (tester) async {
     Widget testWidget = const MediaQuery(
-        data: MediaQueryData(),
-        child: MaterialApp(home: NotesPage()));
+        data: MediaQueryData(), child: MaterialApp(home: NotesPage()));
 
     await tester.pumpWidget(testWidget);
 
-    final buttonPersonalNotesText = find.widgetWithText(TextButton, 'Personal Notes');
+    final buttonPersonalNotesText =
+        find.widgetWithText(TextButton, 'Personal Notes');
 
     expect(buttonPersonalNotesText, findsOneWidget);
   });
-
-  //testing that the 'Logbook' button appears
-  testWidgets('Testing Logbook button appear', (tester) async {
-    Widget testWidget = const MediaQuery(
-        data: MediaQueryData(),
-        child: MaterialApp(home: NotesPage()));
-
-    await tester.pumpWidget(testWidget);
-
-    final buttonLogbookText = find.widgetWithText(TextButton, 'Logbook');
-
-    expect(buttonLogbookText, findsOneWidget);
-  });
-
 }
